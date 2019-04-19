b470626c5d87a57b058c53ff40e098593d80e733
wenchi53 <b99901053@gmail.com>
2019-04-18 22:34:41 -0700

    Add some note

--- note.java
+++ note.java
@@ -1,6 +1,27 @@
-// Hash Map
+/************************
+        Hash Map
+************************/
 Map<Integer, Integer> map = HashMap<>();
 Hash_Map.containsKey(key)
-map.put("value", key);
+map.put(key, value);
+map.get(key);
 
 
+/************************
+          Array
+************************/
+
+int[] nums;
+
+//Create an array
+new int[] {i, j};
+
+int[] ret = new int[2];
+ret[0] = i;
+ret[1] = j;
+
+//Length of array
+nums.length
+
+//Sort an array
+Arrays.sort(num);
